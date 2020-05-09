ec6b2df97591b365e35f14aa651ae5053c9b631a
Stijn Dejongh <stijn.c.dejongh@gmail.com>
2020-05-09 22:06:29 +0200

    Uncle Bob kata end

--- src/main/java/be/doji/sandbox/kata/Game.java
+++ src/main/java/be/doji/sandbox/kata/Game.java
@@ -2,7 +2,11 @@ package be.doji.sandbox.kata;
 
 public class Game {
 
-	private final int[] pinsKnockedOver = new int[21];
+	public static final int MAXIMUM_ROLL_IN_GAME = 21;
+	public static final int AMOUNT_OF_FRAMES_IN_GAME = 10;
+	public static final int MAX_PINS_PER_FRAME = 10;
+
+	private final int[] pinsKnockedOver = new int[MAXIMUM_ROLL_IN_GAME];
 	private int currentRoll = 0;
 
 	public void roll(int pinsRolledOver) {
@@ -12,20 +16,38 @@ public class Game {
 	public int score() {
 		int score = 0;
 		int rollCounter = 0;
-		for(int frame = 0; frame < 10; frame++) {
-			if(pinsKnockedOver[rollCounter] == 10) {
-				score += 10 + pinsKnockedOver[rollCounter +1 ] + pinsKnockedOver[rollCounter + 2];
+		for(int frame = 0; frame < AMOUNT_OF_FRAMES_IN_GAME; frame++) {
+			if(isStrike(rollCounter)) {
+				score += 10 + strikeBonus(rollCounter);
 				rollCounter += 1;
+			} else if(isSpare(rollCounter)) {
+				score += sumOfPinsKnockedOverInFrame(rollCounter) + spareBonus(rollCounter);
+				rollCounter += 2;
 			} else {
-				int frameScore = pinsKnockedOver[rollCounter] + pinsKnockedOver[rollCounter + 1];
-				score += isSpare(frameScore) ? frameScore + pinsKnockedOver[rollCounter + 2] : frameScore;
+				score += sumOfPinsKnockedOverInFrame(rollCounter);
 				rollCounter += 2;
 			}
 		}
 		return score;
 	}
 
-	private boolean isSpare(int frameScore) {
-		return frameScore == 10;
+	private boolean isStrike(int rollCounter) {
+		return pinsKnockedOver[rollCounter] == MAX_PINS_PER_FRAME;
+	}
+
+	private boolean isSpare(int rollCounter) {
+		return sumOfPinsKnockedOverInFrame(rollCounter) == MAX_PINS_PER_FRAME;
+	}
+
+	private int sumOfPinsKnockedOverInFrame(int rollCounter) {
+		return pinsKnockedOver[rollCounter] + pinsKnockedOver[rollCounter + 1];
+	}
+
+	private int spareBonus(int rollCounter) {
+		return pinsKnockedOver[rollCounter + 2];
+	}
+
+	private int strikeBonus(int rollCounter) {
+		return pinsKnockedOver[rollCounter + 1] + pinsKnockedOver[rollCounter + 2];
 	}
 }
