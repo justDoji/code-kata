7301fca5f3dab30ce363216fead8844b89811123
Stijn Dejongh <stijn.c.dejongh@gmail.com>
2020-05-09 21:08:11 +0200

    Added next tests -> green

--- src/main/java/be/doji/sandbox/kata/Game.java
+++ src/main/java/be/doji/sandbox/kata/Game.java
@@ -1,4 +1,14 @@
 package be.doji.sandbox.kata;
 
 public class Game {
+
+	private int score = 0;
+
+	public void roll(int pinsRolledOver) {
+		score += pinsRolledOver;
+	}
+
+	public int score() {
+		return this.score;
+	}
 }
--- src/test/java/be/doji/sandbox/kata/BowlingGameTest.java
+++ src/test/java/be/doji/sandbox/kata/BowlingGameTest.java
@@ -1,12 +1,32 @@
 package be.doji.sandbox.kata;
 
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class BowlingGameTest {
 
-	@Test
-	public void gutterGame() {
-		Game g = new Game();
+	private Game game;
+
+	@BeforeEach
+	void setUp() {
+		game = new Game();
 	}
 
+	@Test
+	public void gutterGame() {
+		for(int i = 0; i < 20; i++) {
+			game.roll(0);
+		}
+		assertThat(game.score()).as("Expect the game score to be zero if no pins were rolled over").isEqualTo(0);
+	}
+
+	@Test
+	void allOnes() {
+		for(int i = 0; i < 20; i++) {
+			game.roll(1);
+		}
+		assertThat(game.score()).isEqualTo(20);
+	}
 }
