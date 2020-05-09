060b87ebe2e4e6b5722e9c33b3b19c7ffc341562
Stijn Dejongh <stijn.c.dejongh@gmail.com>
2020-05-09 21:14:29 +0200

    Design flaw detected - safty commit
    * Add test to verify spare behaviour
    * Disabled test while redesign/refactor is in progress

--- src/test/java/be/doji/sandbox/kata/BowlingGameTest.java
+++ src/test/java/be/doji/sandbox/kata/BowlingGameTest.java
@@ -3,6 +3,7 @@ package be.doji.sandbox.kata;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class BowlingGameTest {
@@ -16,17 +17,29 @@ public class BowlingGameTest {
 
 	@Test
 	public void gutterGame() {
-		for(int i = 0; i < 20; i++) {
-			game.roll(0);
-		}
+		knockOverPinsOnEveryRolls(20, 0);
 		assertThat(game.score()).as("Expect the game score to be zero if no pins were rolled over").isEqualTo(0);
 	}
 
 	@Test
 	void allOnes() {
-		for(int i = 0; i < 20; i++) {
-			game.roll(1);
-		}
+		knockOverPinsOnEveryRolls(20, 1);
 		assertThat(game.score()).isEqualTo(20);
+	}
+
+	@Test
+	@Disabled("Test brought out design flaw. Redesigning system before enabling test")
+	void oneSpare() {
+		game.roll(5);
+		game.roll(5); //spare
+		game.roll(3);
+		knockOverPinsOnEveryRolls(17, 0);
+		assertThat(game.score()).isEqualTo(16);
+	}
+
+	private void knockOverPinsOnEveryRolls(int amountOfRolls, int pinsToRollOnEachTurn) {
+		for (int i = 0; i < amountOfRolls; i++) {
+			game.roll(pinsToRollOnEachTurn);
+		}
 	}
 }
